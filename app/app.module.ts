4605e979a31d606e22fca0fd95a74696e1e284f3
Santhoshkumar-p <sanssoft12@gmail.com>
2017-07-15 19:35:30 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { displayPublic } from './display-public.pipe'
+import { Truncate } from './truncate.pipe'
+import { FilterNames } from './filter-names.pipe'
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, FilterNames],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+          
---
+++ app/filter-names.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name : "filterNames",
+    pure: false
+})
+
+export class FilterNames implements PipeTransform{
+
+    transform(items: any[], args: string[]): any {
+        return items.filter(item => item.name.charAt(0) === args[0])
+    }
+}
+              
